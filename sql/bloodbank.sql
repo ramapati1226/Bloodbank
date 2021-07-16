0f29aebfe2bd8bf9f96b73534fb37b1c53640fae
ramapati26 <70839204+ramapati26@users.noreply.github.com>
2021-07-16 14:43:16 +0530

    Added composer.json

---
+++ sql/bloodbank.sql
@@ -0,0 +1,170 @@
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+
+--
+-- Database: `bloodbank`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bloodinfo`
+--
+
+CREATE TABLE `bloodinfo` (
+  `bid` int(11) NOT NULL,
+  `hid` int(11) NOT NULL,
+  `bg` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `bloodinfo`
+--
+
+INSERT INTO `bloodinfo` (`bid`, `hid`, `bg`) VALUES
+(1, 1, 'B+'),
+(2, 2, 'A+');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bloodrequest`
+--
+
+CREATE TABLE `bloodrequest` (
+  `reqid` int(11) NOT NULL,
+  `hid` int(11) NOT NULL,
+  `rid` int(11) NOT NULL,
+  `bg` varchar(11) NOT NULL,
+  `status` varchar(100) NOT NULL DEFAULT 'Pending'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `bloodrequest`
+--
+
+INSERT INTO `bloodrequest` (`reqid`, `hid`, `rid`, `bg`, `status`) VALUES
+(1, 1, 1, 'B+', 'Pending');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hospitals`
+--
+
+CREATE TABLE `hospitals` (
+  `id` int(11) NOT NULL,
+  `hname` varchar(100) NOT NULL,
+  `hemail` varchar(100) NOT NULL,
+  `hpassword` varchar(100) NOT NULL,
+  `hphone` varchar(100) NOT NULL,
+  `hcity` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `hospitals`
+--
+
+INSERT INTO `hospitals` (`id`, `hname`, `hemail`, `hpassword`, `hphone`, `hcity`) VALUES
+(1, 'any hospital', 'any@gmail.com', 'anyhospital', '6394313916', 'unknown'),
+(2, 'Unknown hospital', 'unknown@gmail.com', 'unknown', '6394313916', 'unknown');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `receivers`
+--
+
+CREATE TABLE `receivers` (
+  `id` int(11) NOT NULL,
+  `rname` varchar(100) NOT NULL,
+  `remail` varchar(100) NOT NULL,
+  `rpassword` varchar(100) NOT NULL,
+  `rphone` varchar(100) NOT NULL,
+  `rbg` varchar(10) NOT NULL,
+  `rcity` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `receivers`
+--
+
+INSERT INTO `receivers` (`id`, `rname`, `remail`, `rpassword`, `rphone`, `rbg`, `rcity`) VALUES
+(1, 'test', 'test@gmail.com', 'test', '6394313916', 'A+', 'unknown'),
+(2, 'xyz', 'xyz@gmail.com', 'xyz', '6394313916', 'AB+', 'unknown');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `bloodinfo`
+--
+ALTER TABLE `bloodinfo`
+  ADD PRIMARY KEY (`bid`),
+  ADD KEY `hid` (`hid`);
+
+--
+-- Indexes for table `bloodrequest`
+--
+ALTER TABLE `bloodrequest`
+  ADD PRIMARY KEY (`reqid`),
+  ADD KEY `hid` (`hid`);
+
+--
+-- Indexes for table `hospitals`
+--
+ALTER TABLE `hospitals`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `hemail` (`hemail`);
+
+--
+-- Indexes for table `receivers`
+--
+ALTER TABLE `receivers`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `remail` (`remail`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `bloodinfo`
+--
+ALTER TABLE `bloodinfo`
+  MODIFY `bid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `bloodrequest`
+--
+ALTER TABLE `bloodrequest`
+  MODIFY `reqid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `hospitals`
+--
+ALTER TABLE `hospitals`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `receivers`
+--
+ALTER TABLE `receivers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `bloodinfo`
+--
+ALTER TABLE `bloodinfo`
+  ADD CONSTRAINT `bloodinfo_ibfk_1` FOREIGN KEY (`hid`) REFERENCES `hospitals` (`id`);
+COMMIT;
